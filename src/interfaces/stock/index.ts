f447e2cb78e5e37024150c3e864fb493a281ba8d
564648531215156918273645 <33358726+564648531215156918273645@users.noreply.github.com>
2018-11-06 21:17:54 +0000

    Delete index.ts

--- src/interfaces/stock/index.ts
+++
@@ -1,5 +0,0 @@
-import { stockInit as gdax } from './gdax';
-import { stockInit as hitbtc } from './hitbtc';
-import { stockInit as binance } from './binance';
-
-export default { gdax, hitbtc, binance };
